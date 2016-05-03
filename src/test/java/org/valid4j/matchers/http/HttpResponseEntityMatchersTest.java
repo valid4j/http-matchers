304c9d9222515e7f7260ff945ebfb35ca1581bf4
Patrik Helsing <patrik.helsing@gmail.com>
2016-05-03 21:29:29 +0200

    Switch to dropwizard for testing response entities

--- src/test/java/org/valid4j/matchers/http/HttpResponseEntityMatchersTest.java
+++ src/test/java/org/valid4j/matchers/http/HttpResponseEntityMatchersTest.java
@@ -1,16 +1,15 @@
 package org.valid4j.matchers.http;
 
-import com.github.restdriver.clientdriver.ClientDriverRule;
-import org.junit.Rule;
+import io.dropwizard.testing.junit.DropwizardClientRule;
+import org.junit.ClassRule;
 import org.junit.Test;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import static com.github.restdriver.clientdriver.RestClientDriver.giveResponse;
-import static com.github.restdriver.clientdriver.RestClientDriver.onRequestTo;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
 import static org.valid4j.matchers.http.HttpResponseMatchers.hasContentLength;
@@ -20,32 +19,33 @@ import static org.valid4j.matchers.http.helpers.MatcherMatchers.isDescribedBy;
 
 public class HttpResponseEntityMatchersTest {
 
-    @Rule
-    public final ClientDriverRule service = new ClientDriverRule();
+    @Path("/")
+    public static class PayloadResource {
+        @GET
+        public String payload() {
+            return "some-content-as-payload";
+        }
+    }
+
+    @ClassRule
+    public final static DropwizardClientRule server = new DropwizardClientRule(new PayloadResource());
+
     public final Client client = ClientBuilder.newClient();
 
     @Test
     public void shouldMatchByEntityWithStringValue() {
-        service.addExpectation(
-                onRequestTo("/"),
-                giveResponse("payload", MediaType.TEXT_PLAIN).withStatus(200));
-
-        Response response = client.target(service.getBaseUrl()).request().get();
-        assertThat(response, hasEntity(String.class, equalTo("payload")));
-        assertThat(response, hasEntity(equalTo("payload")));
+        Response response = client.target(server.baseUri()).request().get();
+        assertThat(response, hasEntity(String.class, equalTo("some-content-as-payload")));
+        assertThat(response, hasEntity(equalTo("some-content-as-payload")));
         assertThat(hasEntity(String.class, equalTo("payload")),
             isDescribedBy("has Entity \"payload\""));
       assertThat(mismatchOf(response, hasEntity(equalTo("some-other"))),
-          equalTo("Entity was \"payload\""));
+              equalTo("Entity was \"some-content-as-payload\""));
     }
 
     @Test
     public void shouldMatchByContentLength() {
-        service.addExpectation(
-                onRequestTo("/"),
-                giveResponse("some-content-as-payload", MediaType.TEXT_PLAIN).withStatus(200));
-
-        Response response = client.target(service.getBaseUrl()).request().get();
+        Response response = client.target(server.baseUri()).request().get();
         assertThat(response, hasContentLength(equalTo(23)));
         assertThat(hasContentLength(equalTo(7)),
                 isDescribedBy("has Content-Length <7>"));
